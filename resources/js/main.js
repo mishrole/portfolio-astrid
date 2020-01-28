bb7878c2a600df36349257cc76846efe6b80348a
Mitchell Rodríguez <mitrodle@gmail.com>
2020-01-28 04:32:05 -0500

    dee

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
+
     // Seteando intervalo del carousel a 4segundos e inhabilitando la pausa al hacer hover
     $('.carousel').carousel({
         interval: 4000,
         pause: false
     })
 
+    $('.nav-link').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
+	});
+
+
     const home = $("#home").offset().top;
-    const servicios = $("#servicios").offset().top - 60;
+    const portafolio = $("#portafolio").offset().top - 60;
     
     $(document).scroll(function() {
         const scrollPosicion = $(document).scrollTop();
     
-        if(scrollPosicion >= home && scrollPosicion < servicios) {
+        if(scrollPosicion >= home && scrollPosicion < portafolio) {
             $('nav.navbar').removeClass("dark");
-        }else if(scrollPosicion >= servicios){
+            $('a.nav-link').first().removeClass('orange');
+        }else if(scrollPosicion >= portafolio){
             $('nav.navbar').addClass("dark");
+            $('a.nav-link').first().addClass('orange');
         }
     });
 

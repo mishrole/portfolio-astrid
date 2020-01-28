e0911982fdad7e372ccf2b860cfa2c39339a6e62
Mitchell Rodríguez <mitrodle@gmail.com>
2020-01-28 09:37:11 -0500

    Scroll - navbar color

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,17 +13,23 @@ $(document).ready(function() {
 
 
     const home = $("#home").offset().top;
+    const servicios = $('#servicios').offset().top - 60;
     const portafolio = $("#portafolio").offset().top - 60;
     
     $(document).scroll(function() {
         const scrollPosicion = $(document).scrollTop();
     
-        if(scrollPosicion >= home && scrollPosicion < portafolio) {
+        if(scrollPosicion >= home && scrollPosicion < servicios) {
             $('nav.navbar').removeClass("dark");
-            $('a.nav-link').first().removeClass('orange');
-        }else if(scrollPosicion >= portafolio){
+            $('a.nav-link').eq(0).removeClass('orange');
+            $('a.nav-link').eq(1).removeClass('orange');
+        }else if(scrollPosicion >= servicios && scrollPosicion < portafolio){
             $('nav.navbar').addClass("dark");
-            $('a.nav-link').first().addClass('orange');
+            $('a.nav-link').eq(1).removeClass('orange');
+            $('a.nav-link').eq(0).addClass('orange');
+        }else if(scrollPosicion >= portafolio) {
+            $('a.nav-link').eq(0).removeClass('orange');
+            $('a.nav-link').eq(1).addClass('orange');
         }
     });
 

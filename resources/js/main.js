6f83297b0cbf688cc8d89267d96feeb5bfd72be7
Mitchell Rodríguez <mitrodle@gmail.com>
2020-01-28 01:18:37 -0500

    Home + Navbar Scroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
+    // Seteando intervalo del carousel a 4segundos e inhabilitando la pausa al hacer hover
     $('.carousel').carousel({
-        interval: 3000
+        interval: 4000,
+        pause: false
     })
+
+    const home = $("#home").offset().top;
+    const servicios = $("#servicios").offset().top - 60;
+    
+    $(document).scroll(function() {
+        const scrollPosicion = $(document).scrollTop();
+    
+        if(scrollPosicion >= home && scrollPosicion < servicios) {
+            $('nav.navbar').removeClass("dark");
+        }else if(scrollPosicion >= servicios){
+            $('nav.navbar').addClass("dark");
+        }
+    });
+
+
 });
